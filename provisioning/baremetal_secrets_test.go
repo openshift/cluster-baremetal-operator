482e1bab1fe438470f3afae3d482a5ec69ad43cc
Sandhya Dasu <sdasu@redhat.com>
2020-10-05 18:13:29 -0400

    Augmented unit tests for baremetal secrets

--- provisioning/baremetal_secrets_test.go
+++ provisioning/baremetal_secrets_test.go
@@ -5,10 +5,13 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/net/context"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	fakekube "k8s.io/client-go/kubernetes/fake"
+	faketesting "k8s.io/client-go/testing"
 )
 
 const testNamespace = "test-namespce"
@@ -29,79 +32,90 @@ func TestGenerateRandomPassword(t *testing.T) {
 	}
 }
 
-// Testing the case where the Mariadb password already exists
-// First we create a Mariadb Password using the method being tested.
-// Then we attenpt to create it again by calling the method again.
-// Instead of creating a new one, it should return the pre-existing secret
 func TestCreateMariadbPasswordSecret(t *testing.T) {
-	kubeClient := fakekube.NewSimpleClientset(nil...)
-	client := kubeClient.CoreV1()
 
-	// First create a mariadb password secret
-	if err := CreateMariadbPasswordSecret(kubeClient.CoreV1(), testNamespace); err != nil {
-		t.Fatalf("Failed to create first Mariadb password. %s ", err)
+	cases := []struct {
+		name          string
+		secretError   *errors.StatusError
+		expectedError error
+		testRecreate  bool
+	}{
+		{
+			name:          "new-mariadb-secret",
+			expectedError: nil,
+			testRecreate:  true,
+		},
+		{
+			name:          "new-ironic-secret",
+			expectedError: nil,
+		},
+		{
+			name:          "new-inspector-secret",
+			expectedError: nil,
+		},
+		{
+			name:          "error-fetching-secret",
+			secretError:   errors.NewServiceUnavailable("an error"),
+			expectedError: errors.NewServiceUnavailable("an error"),
+		},
 	}
-	// Read and get Mariadb password from Secret just created.
-	oldMariadbPassword, err := client.Secrets(testNamespace).Get(context.Background(), baremetalSecretName, metav1.GetOptions{})
-	if err != nil {
-		t.Fatal("Failure getting the first Mariadb password that just got created.")
-	}
-	oldPassword, ok := oldMariadbPassword.StringData[baremetalSecretKey]
-	if !ok || oldPassword == "" {
-		t.Fatal("Failure reading first Mariadb password from Secret.")
-	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			secretsResource := schema.GroupVersionResource{Group: "", Version: "v1", Resource: "secrets"}
 
-	// The pasword definitely exists. Try creating again.
-	if err := CreateMariadbPasswordSecret(kubeClient.CoreV1(), testNamespace); err != nil {
-		t.Fatal("Failure creating second Mariadb password.")
-	}
-	newMariadbPassword, err := client.Secrets(testNamespace).Get(context.Background(), baremetalSecretName, metav1.GetOptions{})
-	if err != nil {
-		t.Fatal("Failure getting the second Mariadb password.")
-	}
-	newPassword, ok := newMariadbPassword.StringData[baremetalSecretKey]
-	if !ok || newPassword == "" {
-		t.Fatal("Failure reading second Mariadb password from Secret.")
-	}
-	if oldPassword != newPassword {
-		t.Fatalf("Both passwords do not match.")
-	} else {
-		t.Logf("First Mariadb password is being preserved over re-creation as expected.")
-	}
-}
+			kubeClient := fakekube.NewSimpleClientset(nil...)
 
-func TestCreateIronicPasswordSecret(t *testing.T) {
-	kubeClient := fakekube.NewSimpleClientset(nil...)
-	client := kubeClient.CoreV1()
+			if tc.secretError != nil {
+				kubeClient.Fake.PrependReactor("get", "secrets", func(action faketesting.Action) (handled bool, ret runtime.Object, err error) {
+					return true, &v1.Secret{}, tc.secretError
+				})
+			}
 
-	err := CreateIronicPasswordSecret(client, testNamespace)
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-		return
-	}
-	// Check if Ironic secret exits
-	secret, err := client.Secrets(testNamespace).Get(context.Background(), ironicSecretName, metav1.GetOptions{})
-	if apierrors.IsNotFound(err) {
-		t.Errorf("Error creating Ironic secret.")
-	}
-	assert.True(t, strings.Compare(secret.StringData[ironicUsernameKey], ironicUsername) == 0, "ironic password created incorrectly")
-	return
-}
+			switch tc.name {
+			case "new-mariadb-secret":
+				err := CreateMariadbPasswordSecret(kubeClient.CoreV1(), testNamespace)
+				assert.Equal(t, tc.expectedError, err)
 
-func TestCreateInspectorPasswordSecret(t *testing.T) {
-	kubeClient := fakekube.NewSimpleClientset(nil...)
-	client := kubeClient.CoreV1()
+				if tc.expectedError == nil {
+					secret, _ := kubeClient.Tracker().Get(secretsResource, testNamespace, "metal3-mariadb-password")
+					assert.NotEmpty(t, secret.(*v1.Secret).StringData[baremetalSecretKey])
+					// Test for making sure that when a secret already exists, a new one is not
+					// created and the old one returned.
+					if tc.testRecreate {
+						original := secret.(*v1.Secret).StringData[baremetalSecretKey]
+						err := CreateMariadbPasswordSecret(kubeClient.CoreV1(), testNamespace)
+						assert.Equal(t, tc.expectedError, err)
+						newSecret, _ := kubeClient.Tracker().Get(secretsResource, testNamespace, "metal3-mariadb-password")
+						recreated := newSecret.(*v1.Secret).StringData[baremetalSecretKey]
+						assert.True(t, strings.Compare(original, recreated) == 0, "re-created mariadb password is invalid")
+					}
+				}
+			case "new-ironic-secret":
+				err := CreateIronicPasswordSecret(kubeClient.CoreV1(), testNamespace)
+				if err != nil {
+					t.Errorf("unexpected error: %v", err)
+					return
+				}
+				// Check if Ironic secret exits
+				secret, err := kubeClient.Tracker().Get(secretsResource, testNamespace, ironicSecretName)
+				if apierrors.IsNotFound(err) {
+					t.Errorf("Error creating Ironic secret.")
+				}
+				assert.True(t, strings.Compare(secret.(*v1.Secret).StringData[ironicUsernameKey], ironicUsername) == 0, "ironic password created incorrectly")
+			case "new-inspector-secret":
+				err := CreateInspectorPasswordSecret(kubeClient.CoreV1(), testNamespace)
+				if err != nil {
+					t.Errorf("unexpected error: %v", err)
+					return
+				}
+				// Check if Ironic secret exits
+				secret, err := kubeClient.Tracker().Get(secretsResource, testNamespace, inspectorSecretName)
+				if apierrors.IsNotFound(err) {
+					t.Errorf("Error creating Ironic secret.")
+				}
+				assert.True(t, strings.Compare(secret.(*v1.Secret).StringData[ironicUsernameKey], inspectorUsername) == 0, "inspector password created incorrectly")
 
-	err := CreateInspectorPasswordSecret(client, testNamespace)
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-		return
+			}
+		})
 	}
-	// Check if Ironic Inspector secret exits
-	secret, err := client.Secrets(testNamespace).Get(context.Background(), inspectorSecretName, metav1.GetOptions{})
-	if apierrors.IsNotFound(err) {
-		t.Errorf("Error creating Ironic Inspector secret.")
-	}
-	assert.True(t, strings.Compare(secret.StringData[ironicUsernameKey], inspectorUsername) == 0, "inspector password created incorrectly")
-	return
 }
