cb2e945c143984563f9e489ba132e5664d30c968
Sandhya Dasu <sdasu@redhat.com>
2020-10-08 10:59:58 -0400

    Read images required for metal3 deployment from configmap

---
+++ controllers/container_images_test.go
@@ -0,0 +1,57 @@
+package controllers
+
+import (
+	"testing"
+)
+
+var (
+	TestImagesFile                = "sample_images.json"
+	expectedBaremetalOperator     = "registry.svc.ci.openshift.org/openshift:baremetal-operator"
+	expectedIronic                = "registry.svc.ci.openshift.org/openshift:ironic"
+	expectedIronicInspector       = "registry.svc.ci.openshift.org/openshift:ironic-inspector"
+	expectedIronicIpaDownloader   = "registry.svc.ci.openshift.org/openshift:ironic-ipa-downloader"
+	expectedMachineOsDownloader   = "registry.svc.ci.openshift.org/openshift:ironic-machine-os-downloader"
+	expectedIronicStaticIpManager = "registry.svc.ci.openshift.org/openshift:ironic-static-ip-manager"
+)
+
+func TestGetContainerImages(t *testing.T) {
+	testCases := []struct {
+		name           string
+		imagesFile     string
+		expectedError  bool
+		expectedImages bool
+	}{
+		{
+			name:           "Valid Images File",
+			imagesFile:     TestImagesFile,
+			expectedError:  false,
+			expectedImages: true,
+		},
+		{
+			name:           "Invalid Images File",
+			imagesFile:     "invalid.json",
+			expectedError:  true,
+			expectedImages: false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(string(tc.name), func(t *testing.T) {
+			var containerImages Images
+
+			err := GetContainerImages(&containerImages, tc.imagesFile)
+			if tc.expectedError != (err != nil) {
+				t.Errorf("ExpectedError: %v, got: %v", tc.expectedError, err)
+			}
+			if tc.expectedImages {
+				if containerImages.BaremetalOperator != expectedBaremetalOperator ||
+					containerImages.BaremetalIronic != expectedIronic ||
+					containerImages.BaremetalIronicInspector != expectedIronicInspector ||
+					containerImages.BaremetalIpaDownloader != expectedIronicIpaDownloader ||
+					containerImages.BaremetalMachineOsDownloader != expectedMachineOsDownloader ||
+					containerImages.BaremetalStaticIpManager != expectedIronicStaticIpManager {
+					t.Errorf("failed GetContainerImages. One or more Baremetal container images do not match the expected images.")
+				}
+			}
+		})
+	}
+}
