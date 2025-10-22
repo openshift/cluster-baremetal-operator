0f2549187b6e3c956733f65c0735c8920d7532f8
Honza Pokorny <honza@redhat.com>
2025-10-22 19:11:27 -0300

    Mount /etc/pki/ca-trust to machine-os-images
    Instead of mounting the trusted bundle from the ConfigMap provided by cluster
    network operator, we mount the whole `/etc/pki/ca-trust` directory from
    the host.  This directory includes both the ca bundle and the user
    bundle.  Mounting the whole directory also means that we don't have to
    worry about running `update-ca-trust`.

--- provisioning/baremetal_pod.go
+++ provisioning/baremetal_pod.go
@@ -117,7 +117,7 @@ var metal3Volumes = []corev1.Volume{
 	},
 	imageVolume(),
 	ironicAgentPullSecretVolume(),
-	userCABundleVolume(),
+	caTrustDirVolume(),
 	{
 		Name: ironicCredentialsVolume,
 		VolumeSource: corev1.VolumeSource{
