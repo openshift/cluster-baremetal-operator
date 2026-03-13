fbe58fce95eb609aa175140206ab6ca7538793db
Himanshu Roy <hroy@redhat.com>
2026-03-13 15:03:55 +0530

    Older ocp releases use pointer library, update manifests
    (cherry picked from commit d9073c70d2ddc8544c4baad735816a6d42d63363)

--- provisioning/image_customization.go
+++ provisioning/image_customization.go
@@ -142,7 +142,7 @@ func createImageCustomizationContainer(images *Images, info *ProvisioningInfo, i
 		// TODO: This container does not have to run in privileged mode when the i-c-c has
 		// its own volume and does not have to use the imageCacheSharedVolume
 		SecurityContext: &corev1.SecurityContext{
-			ReadOnlyRootFilesystem: ptr.To(false),
+			ReadOnlyRootFilesystem: pointer.BoolPtr(false),
 			// Needed for hostPath image volume mount
 			Privileged: pointer.BoolPtr(true),
 			Capabilities: &corev1.Capabilities{
