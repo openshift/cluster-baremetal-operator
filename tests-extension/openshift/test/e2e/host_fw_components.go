0c5cb7c5ae7043f52f3a331591a1e47249accb26
Jad Haj Yahya <jhajyahy@redhat.com>
2026-06-27 15:18:57 +0300

    Make host_fw_components tests robust and fix nginx pod

--- tests-extension/openshift/test/e2e/host_fw_components.go
+++ tests-extension/openshift/test/e2e/host_fw_components.go
@@ -1,6 +1,7 @@
 package baremetal
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -51,7 +52,8 @@ var _ = g.Describe("[OTP][sig-baremetal] INSTALLER IPI for INSTALLER_DEDICATED j
 		compat_otp.By("Add a label to first worker node ")
 		workerNode, err := compat_otp.GetClusterNodesBy(oc, "worker")
 		o.Expect(err).NotTo(o.HaveOccurred())
-		compat_otp.AddLabelToNode(oc, workerNode[0], "nginx-node", "true")
+		nginxNode := workerNode[0]
+		compat_otp.AddLabelToNode(oc, nginxNode, "nginx-node", "true")
 
 		compat_otp.By("Create web-server to host the fw file")
 		BaseDir := compat_otp.FixturePath("testdata", "installer")
@@ -110,7 +112,7 @@ var _ = g.Describe("[OTP][sig-baremetal] INSTALLER IPI for INSTALLER_DEDICATED j
 			patchConfig := `[{"op": "replace", "path": "/spec/updates", "value": []}]`
 			patchErr := oc.AsAdmin().WithoutNamespace().Run("patch").Args("HostFirmwareComponents", "-n", machineAPINamespace, host, "--type=json", "-p", patchConfig).Execute()
 			o.Expect(patchErr).NotTo(o.HaveOccurred())
-			compat_otp.DeleteLabelFromNode(oc, workerNode[0], "nginx-node")
+			compat_otp.DeleteLabelFromNode(oc, nginxNode, "nginx-node")
 			nodeHealthErr := clusterNodesHealthcheck(oc, 1500)
 			compat_otp.AssertWaitPollNoErr(nodeHealthErr, "Cluster did not recover in time!")
 			clusterOperatorHealthcheckErr := clusterOperatorHealthcheck(oc, 1500, dirname)
@@ -259,12 +261,60 @@ var _ = g.Describe("[OTP][sig-baremetal] INSTALLER IPI for INSTALLER_DEDICATED j
 	// port=unknown - no data in BigQuery last 60 days
 	g.It("Author:jhajyahy-Longduration-NonPreRelease-Medium-78361-DAY2 Update host FW via HostFirmwareComponents CRD [Disruptive]", func() {
 		dirname = "OCP-78361.log"
-		host, getBmhErr := oc.AsAdmin().WithoutNamespace().Run("get").Args("bmh", "-n", machineAPINamespace, "-o=jsonpath={.items[4].metadata.name}").Output()
-		if getBmhErr != nil || host == "" {
-			g.Skip("Not enough BareMetalHosts (need at least 5) to run this test")
-		}
-		vendor, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("bmh", "-n", machineAPINamespace, host, "-o=jsonpath={.status.hardware.firmware.bios.vendor}").Output()
+
+		// Dynamically find a provisioned worker BMH instead of hardcoding items[4]
+		compat_otp.By("Find a provisioned worker BareMetalHost for testing")
+		bmhList, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("bmh", "-n", machineAPINamespace, "-o=json").Output()
 		o.Expect(err).NotTo(o.HaveOccurred())
+
+		var bmhJSON struct {
+			Items []struct {
+				Metadata struct {
+					Name string `json:"name"`
+				} `json:"metadata"`
+				Spec struct {
+					ConsumerRef *struct {
+						Name string `json:"name"`
+					} `json:"consumerRef"`
+				} `json:"spec"`
+				Status struct {
+					Hardware struct {
+						Firmware struct {
+							BIOS struct {
+								Vendor string `json:"vendor"`
+							} `json:"bios"`
+						} `json:"firmware"`
+					} `json:"hardware"`
+				} `json:"status"`
+			} `json:"items"`
+		}
+		err = json.Unmarshal([]byte(bmhList), &bmhJSON)
+		o.Expect(err).NotTo(o.HaveOccurred())
+
+		var host, vendor, machine, nodeName string
+		for _, bmh := range bmhJSON.Items {
+			if bmh.Spec.ConsumerRef != nil && bmh.Spec.ConsumerRef.Name != "" {
+				// Get machine to check if it's a worker
+				machineRole, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("machines.machine.openshift.io", "-n", machineAPINamespace, bmh.Spec.ConsumerRef.Name, "-o=jsonpath={.metadata.labels.machine\\.openshift\\.io/cluster-api-machine-role}").Output()
+				if err == nil && machineRole == "worker" {
+					host = bmh.Metadata.Name
+					vendor = bmh.Status.Hardware.Firmware.BIOS.Vendor
+					machine = bmh.Spec.ConsumerRef.Name
+					// Get the node name for this BMH
+					nodeName, err = oc.AsAdmin().WithoutNamespace().Run("get").Args("machines.machine.openshift.io", "-n", machineAPINamespace, machine, "-o=jsonpath={.status.nodeRef.name}").Output()
+					if err == nil && nodeName != "" {
+						break
+					}
+				}
+			}
+		}
+
+		if host == "" || nodeName == "" {
+			g.Skip("No suitable provisioned worker BareMetalHost found for firmware update test")
+		}
+
+		e2e.Logf("Selected BMH: %s, Node: %s, Vendor: %s", host, nodeName, vendor)
+
 		initialVersion, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("HostFirmwareComponents", "-n", machineAPINamespace, host, "-o=jsonpath={.status.components[1].currentVersion}").Output()
 		o.Expect(err).NotTo(o.HaveOccurred())
 
@@ -294,18 +344,23 @@ var _ = g.Describe("[OTP][sig-baremetal] INSTALLER IPI for INSTALLER_DEDICATED j
 
 		downloadUrl, fileName := buildFirmwareURL(vendor, initialVersion)
 
-		// Get node name from BMH mapping for reboot status checking
-		compat_otp.By("Get node name from BMH mapping")
-		machine, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("bmh", "-n", machineAPINamespace, host, "-o=jsonpath={.spec.consumerRef.name}").Output()
+		// Select a worker node for nginx that's different from the node being rebooted
+		compat_otp.By("Select a worker node for nginx pod (different from node being rebooted)")
+		workerNodes, err := compat_otp.GetClusterNodesBy(oc, "worker")
 		o.Expect(err).NotTo(o.HaveOccurred())
-		nodeName, err := oc.AsAdmin().WithoutNamespace().Run("get").Args("machines.machine.openshift.io", "-n", machineAPINamespace, machine, "-o=jsonpath={.status.nodeRef.name}").Output()
-		o.Expect(err).NotTo(o.HaveOccurred())
+		o.Expect(len(workerNodes)).Should(o.BeNumerically(">=", 2), "Need at least 2 worker nodes for this test")
 
-		// Label worker node 1 to run the web-server hosting the iso
-		compat_otp.By("Add a label to first worker node ")
-		workerNode, err := compat_otp.GetClusterNodesBy(oc, "worker")
-		o.Expect(err).NotTo(o.HaveOccurred())
-		compat_otp.AddLabelToNode(oc, workerNode[0], "nginx-node", "true")
+		var nginxNode string
+		for _, node := range workerNodes {
+			if node != nodeName {
+				nginxNode = node
+				break
+			}
+		}
+		o.Expect(nginxNode).ShouldNot(o.BeEmpty(), "Failed to find a worker node different from the one being rebooted")
+		e2e.Logf("Using worker node %s for nginx pod (rebooting node %s)", nginxNode, nodeName)
+
+		compat_otp.AddLabelToNode(oc, nginxNode, "nginx-node", "true")
 
 		compat_otp.By("Create web-server to host the fw file")
 		BaseDir = compat_otp.FixturePath("testdata", "installer")
@@ -364,7 +419,7 @@ var _ = g.Describe("[OTP][sig-baremetal] INSTALLER IPI for INSTALLER_DEDICATED j
 			patchConfig := `[{"op": "replace", "path": "/spec/updates", "value": []}]`
 			patchErr := oc.AsAdmin().WithoutNamespace().Run("patch").Args("HostFirmwareComponents", "-n", machineAPINamespace, host, "--type=json", "-p", patchConfig).Execute()
 			o.Expect(patchErr).NotTo(o.HaveOccurred())
-			compat_otp.DeleteLabelFromNode(oc, workerNode[0], "nginx-node")
+			compat_otp.DeleteLabelFromNode(oc, nginxNode, "nginx-node")
 			nodeHealthErr := clusterNodesHealthcheck(oc, 3000)
 			compat_otp.AssertWaitPollNoErr(nodeHealthErr, "Cluster did not recover in time!")
 			clusterOperatorHealthcheckErr := clusterOperatorHealthcheck(oc, 1500, dirname)
@@ -463,7 +518,8 @@ var _ = g.Describe("[OTP][sig-baremetal] INSTALLER IPI for INSTALLER_DEDICATED j
 		compat_otp.By("Add a label to first worker node ")
 		workerNode, err := compat_otp.GetClusterNodesBy(oc, "worker")
 		o.Expect(err).NotTo(o.HaveOccurred())
-		compat_otp.AddLabelToNode(oc, workerNode[0], "nginx-node", "true")
+		nginxNode := workerNode[0]
+		compat_otp.AddLabelToNode(oc, nginxNode, "nginx-node", "true")
 
 		compat_otp.By("Create web-server to host the fw file")
 		BaseDir = compat_otp.FixturePath("testdata", "installer")
@@ -521,7 +577,7 @@ var _ = g.Describe("[OTP][sig-baremetal] INSTALLER IPI for INSTALLER_DEDICATED j
 			patchConfig := `[{"op": "replace", "path": "/spec/updates", "value": []}]`
 			patchErr := oc.AsAdmin().WithoutNamespace().Run("patch").Args("HostFirmwareComponents", "-n", machineAPINamespace, host, "--type=json", "-p", patchConfig).Execute()
 			o.Expect(patchErr).NotTo(o.HaveOccurred())
-			compat_otp.DeleteLabelFromNode(oc, workerNode[0], "nginx-node")
+			compat_otp.DeleteLabelFromNode(oc, nginxNode, "nginx-node")
 			nodeHealthErr := clusterNodesHealthcheck(oc, 3000)
 			compat_otp.AssertWaitPollNoErr(nodeHealthErr, "Cluster did not recover in time!")
 			clusterOperatorHealthcheckErr := clusterOperatorHealthcheck(oc, 1500, dirname)
@@ -591,7 +647,8 @@ var _ = g.Describe("[OTP][sig-baremetal] INSTALLER IPI for INSTALLER_DEDICATED j
 		compat_otp.By("Add a label to first worker node ")
 		workerNode, err := compat_otp.GetClusterNodesBy(oc, "worker")
 		o.Expect(err).NotTo(o.HaveOccurred())
-		compat_otp.AddLabelToNode(oc, workerNode[0], "nginx-node", "true")
+		nginxNode := workerNode[0]
+		compat_otp.AddLabelToNode(oc, nginxNode, "nginx-node", "true")
 
 		compat_otp.By("Create web-server to host the fw file")
 		BaseDir := compat_otp.FixturePath("testdata", "installer")
@@ -649,7 +706,7 @@ var _ = g.Describe("[OTP][sig-baremetal] INSTALLER IPI for INSTALLER_DEDICATED j
 			patchConfig := `[{"op": "replace", "path": "/spec/updates", "value": []}]`
 			patchErr := oc.AsAdmin().WithoutNamespace().Run("patch").Args("HostFirmwareComponents", "-n", machineAPINamespace, host, "--type=json", "-p", patchConfig).Execute()
 			o.Expect(patchErr).NotTo(o.HaveOccurred())
-			compat_otp.DeleteLabelFromNode(oc, workerNode[0], "nginx-node")
+			compat_otp.DeleteLabelFromNode(oc, nginxNode, "nginx-node")
 			nodeHealthErr := clusterNodesHealthcheck(oc, 3000)
 			compat_otp.AssertWaitPollNoErr(nodeHealthErr, "Cluster did not recover in time!")
 			clusterOperatorHealthcheckErr := clusterOperatorHealthcheck(oc, 1500, dirname)
